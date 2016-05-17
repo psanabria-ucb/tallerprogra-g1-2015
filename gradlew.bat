2e92f848932e4ff4c5bc535d90608f7c365653d3
jasanet <jasa.net15@gmail.com>
2016-05-17 18:37:49 -0400

    cambio

--- gradlew.bat
+++ gradlew.bat
@@ -16,9 +16,6 @@ if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-@rem Find java.exe
-if defined JAVA_HOME goto findJavaFromJavaHome
-
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
 if "%ERRORLEVEL%" == "0" goto init
